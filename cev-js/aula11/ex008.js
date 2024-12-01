8e99cfe53922f524924aec82c6e93bb4b4a9976b
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2024-12-01 10:40:48 -0300

    atualizei o repositório

---
+++ cev-js/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 40
+console.log(`A velocidade do seu veículo é ${vel}Km/h`)
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. Você foi multado(a)!')
+} 
+console.log('Siga em frente e dirija com cuidado! :-D')
