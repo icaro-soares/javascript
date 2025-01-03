321695ad4b81c407896205cbc5dc4bec9aad3fc6
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2025-01-03 09:56:00 -0300

    atualizei o repositório

--- cev-js/aula14ex/ex017/script.js
+++ cev-js/aula14ex/ex017/script.js
@@ -1,3 +1,20 @@
 function gerar() {
-   
+    let num = document.querySelector('#txtnum')
+    let tab = document.querySelector('#seltab')
+
+    if (num.value.length == 0) {
+        alert('Preencha os dados necessários')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ``
+        while (c<=10) {
+            let item = document.createElement('option')
+
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
 }
