a2512ba0e4c651fc486c5470c90b2628f320e045
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2025-01-04 09:59:43 -0300

    atualizei o repositório

---
+++ cev-js/pratica/desafio-tabuada/script.js
@@ -0,0 +1,13 @@
+function gerar() {
+    let num = document.querySelector('#txtnum')
+    let tab = document.querySelector('#seltab')
+
+    if (num.value.length == 0) {
+        alert('Complete os dados, por favor')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+
+        
+    }
+}
