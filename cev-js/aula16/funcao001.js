2b676a5a30d6dc3ba10c36edaad385b52d9d005b
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2025-01-14 10:25:10 -0300

    atualizei o repositório

---
+++ cev-js/aula16/funcao001.js
@@ -0,0 +1,10 @@
+function parimpar(n) {
+    if (n%2==0) {
+        return 'Número par'
+    } else {
+        return 'Número ímpar'
+    }
+}
+
+let res = parimpar(11)
+console.log(res)
---
+++ cev-js/aula16/funcao002.js
@@ -0,0 +1,6 @@
+function soma(n1=0, n2=0, res) {
+    res = n1 + n2
+    return res
+}
+
+console.log(soma(7))
