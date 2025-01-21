6b97b6069d1e2d6fe0d5fd7bb0d078e0a73646b9
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2025-01-21 08:45:43 -0300

    atualizei o repositório

--- cev-js/aula16ex/ex018/script.js
+++ cev-js/aula16ex/ex018/script.js
@@ -1,2 +1,15 @@
-  
+function analisar() {
+    let txn = document.querySelector('#txn')
+    let res = document.querySelector('#res')
+
+    if (txn.value.length == 0) {
+        alert('Reveja os dados')
+    } else {
+        let n = Number(txn.value)
+        if (n <= 0 || n > 100) {
+            alert('Por favor, digite um número en 1 e 100')
+        } else {
+            
+        }
+    }
 }
