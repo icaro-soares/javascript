3890bea04dcc2e66aa1bb814c041fa4aebb43822
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2025-01-21 08:32:52 -0300

    atualizei o repositório

--- cev-js/aula16ex/ex018/script.js
+++ cev-js/aula16ex/ex018/script.js
@@ -1,13 +1,2 @@
-function analisar() {
-    let txnum = document.querySelector('#txnum')
-
-    if (txnum.value.length > 0) {
-        let num = Number(txnum.value)
-        if (num>=0 || num<=100) {
-            alert('Tudo Ok')
-        } else {
-            alert('Reveja os dados')
-        }
-    }
-    
+  
 }
